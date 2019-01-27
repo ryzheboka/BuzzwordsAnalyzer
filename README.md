160278b5609d0a8c40f572e84fff8014530ff480
Elena Mokeeva <ryzheboka@gmail.com>
2019-01-27 13:30:33 +0100

    add printing sample sentences and show them on the plot

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Statistics about texts with occurrence of buzzwords. The prototype takes only th
 - https://www.kaggle.com/quora/question-pairs-dataset
 - https://www.kaggle.com/hsankesara/medium-articles
 ## Current Results
-Need to look up samples from different clusters for interpretation.
+I couldn't find any similarities inside the clusters. The simplification of 2 component PCA from 300 features might be too large. 
 ![alttext](results/comparison_of_rand_sentences_vs_sentences_with_AI_occurrence.png) 
 ## References 
 - stop words from: https://www.ranks.nl/stopwords
--- prototype_for_term_AI/visualize_raw_sentence_vectors.py
+++ prototype_for_term_AI/visualize_raw_sentence_vectors.py
@@ -7,12 +7,10 @@ from matplotlib import pyplot as plt
 
 
 def filter_flatten_pca_transform(matrices,max_size):
-    long_sentences = [np.array(sentence) for i,sentence in enumerate(matrices) if 5 <= len(sentence) <= 10 and i<max_size]
+    long_sentences = [(np.array(sentence)[:5],i) for i,sentence in enumerate(matrices) if 5 <= len(sentence) <= 10 and i<max_size]
     flatten_matrices = np.zeros((len(long_sentences), 1500))
-
     for i, sentence in enumerate(long_sentences):
-        sentence = sentence[:5]
-        sentence = np.concatenate([np.array(word) for word in sentence])
+        sentence = np.concatenate([np.array(word) for word in sentence[0]])
         flatten_matrices[i] = sentence
 
     pca = PCA(n_components=2)
@@ -20,18 +18,20 @@ def filter_flatten_pca_transform(matrices,max_size):
     principal_components = pca.fit_transform(flatten_matrices)
     principal_df = pd.DataFrame(data=principal_components
                                , columns=['principal component 1', 'principal component 2'])
-    return principal_df,len(long_sentences)
+    return principal_df,len(long_sentences),[el[1] for el in long_sentences]
 
 matrices_AI = np.load("../data/sentences_vectors.npy")
 comparison_matrices = np.load("../data/sentences_vectors.npy")
-df_AI,max_len = filter_flatten_pca_transform(matrices_AI,2000)
-df_comparison,_ = filter_flatten_pca_transform(comparison_matrices,max_len)
+raw_textes_AI = np.load("../data/containAI.npy")
+df_AI,max_len,indices = filter_flatten_pca_transform(matrices_AI,2000)
+df_comparison,_,_ = filter_flatten_pca_transform(comparison_matrices,max_len)
 
 fig = plt.figure(figsize = (8,8))
 ax = fig.add_subplot(1,1,1)
 ax.set_xlabel('Principal Component 1', fontsize = 10)
 ax.set_ylabel('Principal Component 2', fontsize = 10)
 ax.set_title("Comparison of random sentences vs sentences with 'AI' occurrence (PCA)", fontsize = 14)
+ax.grid()
 
 ax.scatter(df_AI["principal component 1"],df_AI["principal component 2"]
 , c = 'red'
@@ -39,8 +39,15 @@ ax.scatter(df_AI["principal component 1"],df_AI["principal component 2"]
 ax.scatter(df_comparison["principal component 1"],df_comparison["principal component 2"]
 , c = 'blue'
 , s = 1)
-ax.grid()
+markers = ["d","+","x","|",">","v","<"]
+for i in range(7):
+    ax.scatter(df_AI["principal component 1"][i],df_AI["principal component 2"][i]
+    , c = 'green'
+    , s = 20, marker=markers[i])
+    print("%d:%s"%(i,raw_textes_AI[indices[i]]))
+
+
 ax.legend(["sentences with occurence of the term 'AI'","random sentences"])
 
-plt.savefig("../results/comparison_of_rand_sentences_vs_sentences_with_AI_occurrence")
+plt.savefig("../data/probe")
 plt.show()
